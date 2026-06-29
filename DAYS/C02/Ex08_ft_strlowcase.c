77be67dac74c6001f2ef5b43e9d2aac8e32313be
Adnan-elberkaoui <adnanelb26@gmail.com>
2026-06-29 20:46:19 +0100

    DAYS

--- DAYS/C02/Ex08_ft_strlowcase.c
+++ DAYS/C02/Ex08_ft_strlowcase.c
@@ -6,7 +6,7 @@
 /*   By: cypher <cypher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 20:40:53 by cypher            #+#    #+#             */
-/*   Updated: 2026/06/29 20:43:01 by cypher           ###   ########.fr       */
+/*   Updated: 2026/06/29 20:45:45 by cypher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,21 @@ char *ft_strlowcase(char *str)
      i++;  
     }
     return str ;
+}
+
+#include <stdio.h>
+
+int main (void)
+{
+    char str1[] = "HELLO";
+ft_strlowcase(str1);
+// "hello"
+
+char str2[] = "42 ABC!";
+ft_strlowcase(str2);
+// "42 abc!"
+
+char str3[] = "";
+ft_strlowcase(str3);
+// ""
 }
