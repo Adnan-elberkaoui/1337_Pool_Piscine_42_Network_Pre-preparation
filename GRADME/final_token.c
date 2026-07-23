3cefa7e47f31cc33640716749d770a933a2a08ea
Adnan-elberkaoui <adnanelb26@gmail.com>
2026-07-23 09:49:30 +0100

    GRADME

--- GRADME/final_token.c
+++ GRADME/final_token.c
@@ -1,34 +1,9 @@
-// Write a program that receives exactly one string argument and prints its last word followed by a newline.
-
-// Steps
-
-
-// A word is a sequence of non-whitespace characters. Spaces and tabs act as delimiters.
-
-// Skip any trailing spaces or tabs before locating the last word.
-
-// If the program is called with anything other than exactly one argument, 
-// print wrong number of arguments followed by a newline. If no word is found, print only a newline.
-// examples
-// ./final_token "llm says works on my branch" | cat -e
-
-// branch$
-
-// ./final_token "rebase squash push" | cat -e
-
-// push$
-
-// ./final_token " " | cat -e
-
-// $
-
-
-
 #include <unistd.h>
 
 int	main(int argc, char **argv)
 {
-	int i;
+	int	i;
+	int	end;
 
 	if (argc != 2)
 	{
@@ -40,32 +15,24 @@ int	main(int argc, char **argv)
 	while (argv[1][i])
 		i++;
 
+	/* Skip trailing spaces and tabs */
 	i--;
-
-	// skip trailing spaces and tabs
 	while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t'))
 		i--;
 
-	// no word found
-	if (i < 0)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
+	end = i;
 
-	// find the beginning of the last word
+	/* Find the beginning of the last word */
 	while (i >= 0 && argv[1][i] != ' ' && argv[1][i] != '\t')
 		i--;
 
 	i++;
 
-	// print last word
-	while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+	while (i <= end)
 	{
 		write(1, &argv[1][i], 1);
 		i++;
 	}
-
 	write(1, "\n", 1);
 	return (0);
 }
